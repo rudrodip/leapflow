8e198c5544e088f4cb3d70f17f447888734008ee
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 13:04:15 +0600

    updated usedimension hook

--- src/lib/hooks/useDImension.tsx
+++ src/lib/hooks/useDImension.tsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 
-export default function useDimension() {
-  const [dimension, setDimension] = useState({
-    width: window ? window.innerWidth : 0,
-    height: window ? window.innerHeight : 0,
+const useDimension = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
   });
-
   useEffect(() => {
     const handleResize = () => {
-      setDimension({
+      setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
@@ -21,5 +20,7 @@ export default function useDimension() {
     };
   }, []);
 
-  return dimension;
-}
+  return windowSize;
+};
+
+export default useDimension;
