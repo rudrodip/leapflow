f5c53fcae4d917bb73a516d1f25c9e16fa7f0cee
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 20:40:41 +0600

    init blog categories

---
+++ src/config/blogCategories.config.ts
@@ -0,0 +1,20 @@
+import type { BlogCategory } from "@/types";
+
+export const blogCategories: BlogCategory[] = [
+  {
+    name: "All posts",
+    slug: "/",
+  },
+  {
+    name: "Announcement",
+    slug: "/announcement",
+  },
+  {
+    name: "Company News",
+    slug: "/company-news",
+  },
+  {
+    name: "Engineering",
+    slug: "/engineering",
+  },
+];
