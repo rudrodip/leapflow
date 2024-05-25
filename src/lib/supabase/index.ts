cf5d5df8bf6102a045c8f0b3dbe6da4cbe1c9b7a
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 16:50:13 +0600

    updated env keys

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import { env } from "@/env.mjs";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
