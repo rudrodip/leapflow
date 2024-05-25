c4db837f545a3873151808190cc97b8bd3e1a5c1
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 14:01:50 +0600

    init intigretation cards

---
+++ src/components/integrations/integration-cards/index.tsx
@@ -0,0 +1,7 @@
+import Integration1 from "./integration1";
+import Integration2 from "./integration2";
+import Integration3 from "./integration3";
+import Integration4 from "./integration4";
+import Integration5 from "./integration5";
+
+export { Integration1, Integration2, Integration3, Integration4, Integration5 }
