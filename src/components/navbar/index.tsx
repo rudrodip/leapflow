d4ebdf0bf70e3fbc1b1d5161bc24ee93706dfbfc
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 11:03:34 +0600

    added mobile navigation

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,24 +6,23 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { Icons } from "@/components/icons";
-import { useRouter } from "next/navigation";
+import MobileNavbar from "./mobile-nav";
 
 export default function DefaultNavbar() {
   const pathname = usePathname();
-  const router = useRouter();
 
   return (
     <div className="flex relative justify-between w-full max-w-7xl mx-auto py-4 items-center lg:px-2 px-6 z-50">
       <Link href="/">
         <Icons.Logo />
       </Link>
-      <div className="flex items-center gap-x-3">
-        {defaultNavbarConfig.map((item, index) => {
+      <div className="lg:flex hidden text-sm gap-x-5 justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {defaultNavbarConfig.map(item => {
           return (
             <Link key={item.href} href={item.href}>
               <p
                 className={cn(
-                  "text-sm font-medium text-primary/70 hover:text-primary transition-colors duration-75",
+                  "text-sm text-primary/70 hover:text-primary transition-colors duration-75",
                   pathname === item.href ? "text-primary" : ""
                 )}
               >
@@ -36,11 +35,13 @@ export default function DefaultNavbar() {
       <HoverBorderGradient
         containerClassName="border bg-black/10 dark:bg-white/10 p-0"
         backgroundClassname="bg-black inset-[0.5px]"
+        className="hidden md:block"
       >
         <Link href="/#waitlist">
           Join waitlist
         </Link>
       </HoverBorderGradient>
+      <MobileNavbar />
     </div>
   );
 }
