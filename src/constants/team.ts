29792b7b4e219a391a16945487a2a49545d32836
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 15:05:13 +0600

    team details

---
+++ src/constants/team.ts
@@ -0,0 +1,53 @@
+import type { PersonProfile } from "@/types";
+
+export const team: PersonProfile[] = [
+  {
+    name: "Suhas Sumukh",
+    designation: "Co-founder, CEO",
+    image: "/team/suhas.png",
+    twitter: "https://x.com/suhasasumukh",
+    linkedin: "https://www.linkedin.com/in/suhasasumukh"
+  },
+  {
+    name: "Shoubhit Dash",
+    designation: "Co-founder, CTO",
+    image: "/team/nexxel.jpg",
+    twitter: "https://x.com/nexxeln",
+    linkedin: "https://www.linkedin.com/in/shoubhit-dash"
+  },
+  {
+    name: "Anurag Sharma",
+    designation: "Full-stack Engineer",
+    image: "/team/anurag.png",
+    twitter: "https://x.com/theanuragdev",
+    linkedin: "https://www.linkedin.com/in/theanuragdev"
+  },
+  {
+    name: "Rudro Dip",
+    designation: "Full-stack Engineer",
+    image: "/team/rds.png",
+    twitter: "https://x.com/rds_agi",
+    linkedin: "https://www.linkedin.com/in/rudrodip"
+  },
+  {
+    name: "Akshat Goel",
+    designation: "Full-stack Engineer",
+    image: "/team/akshat.png",
+    twitter: "https://x.com/akshatgoel0",
+    linkedin: "https://www.linkedin.com/in/akshatgoel7"
+  },
+  {
+    name: "Balaji Varatharajan",
+    designation: "AI Engineer",
+    image: "/team/balaji.png",
+    twitter: "https://twitter.com/BalajiAI",
+    linkedin: "https://www.linkedin.com/in/balaji-ai"
+  },
+  {
+    name: "Sahil",
+    designation: "Frontend & Design",
+    image: "/team/sahil.png",
+    twitter: "https://x.com/zambani_sahil",
+    linkedin: "https://www.linkedin.com/in/sahilzambani/"
+  },
+]
