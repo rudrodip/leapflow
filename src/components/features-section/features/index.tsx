d8a65aa8aaffc096be82ea44cbef9b0606cb5694
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 14:01:13 +0600

    init features section

---
+++ src/components/features-section/features/index.tsx
@@ -0,0 +1,7 @@
+import Feature1 from "./feature1";
+import Feature2 from "./feature2";
+import Feature3 from "./feature3";
+import Feature4 from "./feature4";
+import Feature5 from "./feature5";
+
+export { Feature1, Feature2, Feature3, Feature4, Feature5 }
