bfb1796172ed0b785ec84c11e7e795b3af0bc586
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 15:05:03 +0600

    advisor details

---
+++ src/constants/advisor.ts
@@ -0,0 +1,26 @@
+import type { PersonProfile } from "@/types";
+
+export const advisors: PersonProfile[] = [
+  {
+    name: "Harsh Agrawal",
+    designation: "Founder, DevKit",
+    image: "/advisors/harsh.png",
+    twitter: "https://twitter.com/itsharshag",
+    linkedin: "https://www.linkedin.com/in/itsharshag"
+  },
+  {
+    name: "Harsh Bhat",
+    designation: "ML Eng, Sectalab",
+    image: "/advisors/harsh-bhat.png",
+    twitter: "https://twitter.com/harshbhatt7585",
+    linkedin: "https://www.linkedin.com/in/harsh-bhat-8638041b1"
+  },
+  {
+    name: "Dhravya Shah",
+    designation: "Founder, Supermemory",
+    image: "/advisors/dhravya.png",
+    twitter: "https://twitter.com/DhravyaShah",
+    linkedin: "https://www.linkedin.com/in/dhravyashah"
+  },
+
+]
