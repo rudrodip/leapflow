f491f49aee92f05fb014fa15a8af5cfd6b7cf9bb
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 14:00:01 +0600

    completely cloned original react version of landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,17 @@
+import Hero from "@/components/hero";
+import FeaturesSection from "@/components/features-section";
+import IntegrationSection from "@/components/integrations/integration-section";
+import IntegrationBento from "@/components/integrations/integration-bento";
+
 export default function Home() {
   return (
-    <main>
-      hello world
+    <main className="flex flex-col justify-center items-center w-full">
+      <div className="w-full bg-themeBackground mb-12 md:mb-24 lg:mb-[30vh]">
+        <Hero />
+      </div>
+      <FeaturesSection />
+      <IntegrationSection />
+      <IntegrationBento />
     </main>
   );
 }
