6f3e751b6674f6f75f81e36a60056612fa54e053
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-25 10:27:46 +0600

    init marketing config

---
+++ src/config/marketing.config.ts
@@ -0,0 +1,20 @@
+import type { DefaultNavConfig } from "@/types"
+
+export const defaultNavbarConfig: DefaultNavConfig = [
+  {
+    title: "About",
+    href: "/about"
+  },
+  {
+    title: "Integration",
+    href: "/#integration"
+  },
+  {
+    title: "Blog",
+    href: "/blog"
+  },
+  {
+    title: "Changelog",
+    href: "/changelog"
+  }
+]
